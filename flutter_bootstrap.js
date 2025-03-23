1259ab999517930380936981c1b7a47a8d25d95c
Chayan Barman <barmanchayan10@gmail.com>
2025-03-24 03:47:07 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "469968809"
+    serviceWorkerVersion: "88910960"
   }
 });
